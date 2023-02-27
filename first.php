4b05a3e0dc1dc795de0c3af485bcd2c6808d3196
AJGit10 <124658872+AJGit10@users.noreply.github.com>
2023-02-27 12:10:18 +0530

    Add files via upload

--- first.php
+++ first.php
@@ -1,5 +1,183 @@
 <?php
-$text = "AJAY ARUN ZALTE";
-    $age = 23;
-    $Town = "Yeola"
-    ?>  
+//variables
+// $name = "AJAY ARUN ZALTE";
+// $age =25;
+// $town = "YEOLA";
+
+// //echo/print
+
+// echo "<h2>" . $name . "</h2>";
+// echo "Study PHP at " . $town . "<br>";
+// echo "<h2>" . $age . "</h2>"
+
+
+//echo
+// echo "I am Ajay ZALTE <br>";
+// echo "I am About to learn PHP.<br>";
+
+// $name = "AJAY ZALTE";
+// $Age = 25;
+// $Town = "Yeola";
+
+// echo $name;
+// echo "<br>";
+// echo $Town
+// echo "<br>";
+// echo $Age;
+
+
+// echo "<h2>" . $name . "</h2>";
+// echo "My Age is" . $Age . "<br>";
+// echo "My Home Town is" . $Town . "<br>";
+
+
+
+//echo
+// $text = "AJAY ZALTE";
+// $X = 10;
+// $Y = 20;
+
+// echo "<h2>" . $text . "</h2>";
+// echo "<br>";
+// echo $X;
+// echo "<br>";
+// echo $Y;
+// echo "<br>";
+// echo $X + $Y;
+// echo "<br>";
+// echo "I am DOIng a greate job of creating a php files" . $text . "<br>";
+
+
+// print "<h2>" . $text . "</h2>";
+// print "Study php Rupees" . $X . "only  <br>";
+// print "the price of the tutorial is" . $Y . "as per the limited stocks may increase any time. <br>";
+// print "Total Cost" . $X + $Y ;
+
+// $X = "my name is ajay";
+// $Y = "My frends name is YZ MR";
+
+// echo $X ;
+// echo "<br>";
+// echo $Y;
+
+// 
+// $X = 1505;
+// $Y = 2050;
+
+// var_dump($X);
+// echo "<br>";
+// var_dump($Y);
+// echo "<br>";
+// var_dump($X + $Y);
+// echo $X;
+// echo "<br>";
+// echo $Y;
+// echo "<br>";
+// echo $X + $Y ;
+
+//Array
+
+// $cars = array("Volvo","BMW","Toyota","Ford","Mahindra","TATA");
+// var_dump($cars);
+// echo "<br>";
+
+
+// class car {
+//     public $color;
+//     public $model;
+//     public function __construct($color,$model){
+//         $this->color=$color;
+//         $this->model=$model;
+//     }
+//     public function message(){
+//         return "MY CAR IS A" . $this->color. " " . $this->model . "!";
+
+//     }
+// }
+
+// $myCar = new Car("Blue", "volvo");
+// echo $myCar -> message();
+// echo "<br>";
+// $myCar = new Car("Red", "Fortuner");
+// echo $myCar -> message();
+
+
+
+
+
+//strlen()
+// echo strlen("AJAY    ZALTE ");
+
+//word count
+// echo str_word_count("Ajay Arun Zalte");
+ //revers a string
+// 
+
+//replace
+// echo str_replace("world", "Greatfull", "Awesome Life Better Choise");
+
+// CRPF230597466
+
+// 9356163
+
+//numbera
+// $x = 15050;
+//     var_dump(is_int($x));
+//     echo "<br>";
+
+//     $y = 10.54;
+//     var_dump(is_float($y));
+
+//numeric string
+// $x = 1542;
+// $y = 2465;
+// var_dump(is_numeric($x));
+// echo "<br>";
+   // var_dump(is_numeric($y));
+
+
+//casting int to float adn float to int
+// $X = 154.215;
+// $Y = 451545;
+
+// $int_cast = (int)$X;
+// echo $int_cast;
+
+// echo "<br>";
+// $int_cast = (int)$Y;
+// echo $int_cast;
+
+//form Handling
+// echo $_post["name"];
+// echo "<br>";
+// echo $_post["email"];
+ 
+// <html>
+// <head>
+// <title>First Form php</title>
+
+// </head>
+// <body>
+// <form action="welcome.php" method="post">
+// Name: <input type="text" name="name"><br>
+// E-mail: <input type="text" name="email"><br>
+// <input type="submit">
+// </form>
+
+// </body>
+// </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
