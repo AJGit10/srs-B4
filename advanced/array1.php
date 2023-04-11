c8b4019df81c2e305c02cd5c2038b06a776f57f5
AJGit10 <124658872+AJGit10@users.noreply.github.com>
2023-04-11 19:06:11 +0530

    Add files via upload
    Advanced PHP

---
+++ advanced/array1.php
@@ -0,0 +1,318 @@
+<?php
+$str= '[
+    {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Telesolutions 2",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Yodel Voice",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "QuinStreet",
+        "lead_payIn": "44.00"
+    }, {
+        "partner_name": "QuinStreet",
+        "lead_payIn": "40.00"
+    }, {
+        "partner_name": "Education Source",
+        "lead_payIn": "40.00"
+    }, {
+        "partner_name": "Liquid Education",
+        "lead_payIn": "40.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "40.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "40.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "40.00"
+    }, {
+        "partner_name": "Silverback",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "SilverTap LLC",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "SilverTap LLC",
+        "lead_payIn": "30.00"
+    }, {
+        "partner_name": "QuinStreet Web",
+        "lead_payIn": "25.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }, {
+        "partner_name": "Telesolutions",
+        "lead_payIn": "80.00"
+    }]' ;
+
+
+    $array= json_decode($str, TRUE);
+
+    // echo "<pre>"; print_r($array);echo "</pre>";
+
+    for ($i = 0; $i < count($array); $i++) {
+        echo $array[$i] . "<br>";
+    }
+    
+    // Using a foreach loop to print each value of the array
+    foreach ($array as $value) {
+        echo $value . "<br>";
+    }
+?>
---
+++ advanced/dateAndTime.php
@@ -0,0 +1,15 @@
+<?php
+echo "Today is   " . date("d/m/Y") . "<br>";
+echo "Today is " . date("d/m/Y") . "<br>";
+echo "Today is " . date("d-m-Y ") . "<br>";
+echo "Today is " . date("`l`") . "<br>";
+echo "----------------------------------------------------" . "<br>";
+?>
+
+<!-- copyrights -->
+<html>
+    <body>
+        All Copyrights Reserved!!! <br>
+                &copy 2010- <?php echo date("Y"); ?>
+        <body>
+</html>
